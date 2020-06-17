a5a91eafa0adbbb858ce83834c2dbbd875f714f9
Sebastian Marină <sebastian.marina@ulbsibiu.ro>
2020-06-17 20:45:52 +0300

    update route constant

--- server.js
+++ server.js
@@ -40,7 +40,8 @@ app.use(bodyParser.json());
 const users = require("./routes/api/users");
 app.use("/api/users", users);
 
-app.use("/api", require("./routes/api/api"));
+const api = require("./routes/api/api");
+app.use("/api", api);
 
 app.use("/dist", express.static(process.cwd() + "/dist"));
 
